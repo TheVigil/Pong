e18df06931977f53bcbd914793a16925d76fbf24
jwschindler <schindlj@dhbw-loerrach.de>
2018-07-16 13:54:25 +0200

    newest version

--- main/draw_features.py
+++ main/draw_features.py
@@ -4,18 +4,18 @@ import pygame
 import sys
 from pygame.locals import *
 
-LINE_THICKNESS = 20  # line thickness for drawing
+LINE_THICKNESS = 10  # line thickness for drawing
 PADDLE_SIZE = 60  # size of the main paddles
 PADDLE_PADDING = 30  # padding between arena boundaries and the paddles
 
-W_WIDTH = 500
-W_HEIGHT = 400
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 400
 
 # color definitions for drawing in the display window:
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-CANVAS = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
+CANVAS = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 
 def draw_play_area():
@@ -26,22 +26,21 @@ def draw_play_area():
     # ::param:: (tuple w/(0,0), etc...) define the dimension of the rectangle with (0,0) as upper left corner
     # and (W_WIDTH...) as lower right
     # ::param:: (LINE_THICKNESS * 2) ensures the line thickness isn't cut in half by the borders of the window
-    pygame.draw.rect(CANVAS, WHITE, ((0, 0), (W_WIDTH, W_HEIGHT)), (LINE_THICKNESS * 2))
+    pygame.draw.rect(CANVAS, WHITE, ((0, 0), (WINDOW_WIDTH, WINDOW_HEIGHT)), (LINE_THICKNESS * 2))
 
     # use pygame.draw.line(window, color, ((startXY), (endXY)) line_thickness),
     # to draw the center line of our play area - (LINE_THICKNESS/5) ensures the center line is thinner than our borders:
 
-    pygame.draw.line(CANVAS, WHITE, (int(W_WIDTH/2), 0), (int(W_WIDTH/2), (int(W_HEIGHT))), int(LINE_THICKNESS/4))
+    pygame.draw.line(CANVAS, WHITE, (int(WINDOW_WIDTH / 2), 0), (int(WINDOW_WIDTH / 2), (int(WINDOW_HEIGHT))), int(LINE_THICKNESS / 4))
 
 
 def draw_player_paddles(paddle):
 
     # draw the p1 and p2 paddles
-
     # first, ensure the paddle cannot move too low or too high using pygame's
     # built in .top and .bottom attributes for rectangle objects:
-    if paddle.bottom > (W_HEIGHT - LINE_THICKNESS):
-        paddle.bottom = (W_HEIGHT - LINE_THICKNESS)
+    if paddle.bottom > (WINDOW_HEIGHT - LINE_THICKNESS):
+        paddle.bottom = (WINDOW_HEIGHT - LINE_THICKNESS)
     elif paddle.top < LINE_THICKNESS:
         paddle.top = LINE_THICKNESS
 
--- main/pong.py
+++ main/pong.py
@@ -1,5 +1,4 @@
-'''this portion of the program creates a blank screen to eventually house the arena, paddles etc.
-the main game loop is also housed here, within the main() function.'''
+'''the main game loop is housed here, within the main() function.'''
 
 
 # importing from/the pygame library, which will be used to implement main
@@ -18,8 +17,8 @@ from pygame.locals import *
 FRAMES_PER_SECOND = 200
 
 # define the dimensions of the display window
-W_WIDTH = 500
-W_HEIGHT = 400
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 400
 
 
 # main function to handle the initialization of the game. Draws the display window and contains the main game loop
@@ -28,15 +27,20 @@ def main():
 
     pygame.init()  # initialize pygame evironment
     framerate_clock = pygame.time.Clock()  # control the framerate
+
     global DISPLAY
-    DISPLAY = pygame.display.set_mode((W_WIDTH, W_HEIGHT))  # a display surface for pygame to draw on
+    global ball_movement_x
+    ball_movement_x = 1
+    DISPLAY = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))  # a display surface for pygame to draw on
     # draw the ball in its initial position in the XY grid.
-    ball_x_coord = W_WIDTH/2 - draw.LINE_THICKNESS/2  # compensate the center position
-    ball_y_coord = W_WIDTH / 2 - draw.LINE_THICKNESS / 2  # w.r.t. the thickness of our lines
+
+
+    ball_x_coord = WINDOW_WIDTH / 2 - draw.LINE_THICKNESS / 2  # compensate the center position
+    ball_y_coord = WINDOW_HEIGHT / 2 - draw.LINE_THICKNESS / 2  # w.r.t. the thickness of our lines
 
     # define the player paddles in their initial positions
-    p1_position = (W_HEIGHT - draw.PADDLE_SIZE) / 2
-    p2_position = (W_HEIGHT - draw.PADDLE_SIZE) / 2
+    p1_position = (WINDOW_HEIGHT - draw.PADDLE_SIZE) / 2
+    p2_position = (WINDOW_HEIGHT - draw.PADDLE_SIZE) / 2
 
     # create the ball and player paddles using
     # pygame.Rect((X co-ordinate, Y co-ordinate, Width of Rectangle, Length of Rectangle)
@@ -44,7 +48,7 @@ def main():
     p1_paddle = pygame.Rect(draw.PADDLE_PADDING, p1_position, draw.LINE_THICKNESS,
                             draw.PADDLE_SIZE)
 
-    p2_paddle = pygame.Rect((W_WIDTH - draw.PADDLE_PADDING - draw.LINE_THICKNESS),
+    p2_paddle = pygame.Rect((WINDOW_WIDTH - draw.PADDLE_PADDING - draw.LINE_THICKNESS),
                             p2_position, draw.LINE_THICKNESS, draw.PADDLE_SIZE)
 
     # the above definition of the ::param:: X in p2_paddle sets the p2_paddle
@@ -61,21 +65,46 @@ def main():
     pygame.display.set_caption('Pi Pong')  # title the window
 
     while True:  # main loop of the game
+        #  for - loop checks registered events and control flow for them
+        paddles_list = [p1_paddle, p2_paddle]
+        keys = pygame.key.get_pressed()
+        event = pygame.event.poll()
 
-        # drawing of features should be constantly updated while main loop is running
-        # updates occur at every clock tick, as defined below
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        else:  # find currently pressed keys and change paddle position
+            if keys[K_UP]:
+                p2_paddle.y -= 3
+                print("UP")
+                print(keys)
+            if keys[K_DOWN]:
+                p2_paddle.y += 3
+                print("DOWN")
+            if keys[K_w]:
+                p1_paddle.y -= 3
+            if keys[K_s]:
+                p1_paddle.y += 3
+                # drawing of features should be constantly updated while main loop is running
+                # updates occur at every clock tick, as defined below
 
+        for paddle in paddles_list:
+            if ball.colliderect(paddle):
+                if paddle == p1_paddle:
+                    p1_paddle.center
+                    ball_movement_x = 1
+                else:
+                    print("p2")
+                    print(paddle.top)
+                    ball_movement_x = -1
+
+        ball.x += ball_movement_x
+
+        DISPLAY.fill((0, 0, 0))
         draw.draw_play_area()
         draw.draw_player_paddles(p1_paddle)
         draw.draw_player_paddles(p2_paddle)
         draw.draw_ball(ball)
-
-        # for - loop checks registered events for a quit event, allowing the game loop to be exited
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
         pygame.display.update()
 
         framerate_clock.tick(FRAMES_PER_SECOND)  # advance the clock at our desired fps
